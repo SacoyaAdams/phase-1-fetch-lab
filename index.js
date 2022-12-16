21863081897ab58da59afcf4d6461bed926466d0
Sacoya <sacoyaengineer@gmail.com>
2022-12-15 21:32:58 -0500

    commit tree 41c7a0d0935319484ab9596719a567c258450f45

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  console.log("hello")
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => console.log(data));
 }
+renderBooks(books);
 
 function renderBooks(books) {
   const main = document.querySelector('main');
